2661803f88225e2d6bdce4273da371799a332b37
pirjo2 <pirjo.joelo@gmail.com>
2022-10-24 11:48:08 +0300

    DEBUG message

--- Uus kaust/SaleSystemGUI/src/main/java/ee/ut/math/tvt/salessystem/ui/controllers/TeamController.java
+++ Uus kaust/SaleSystemGUI/src/main/java/ee/ut/math/tvt/salessystem/ui/controllers/TeamController.java
@@ -10,9 +10,10 @@ import java.util.logging.Logger;
 //import org.apache.logging.log4j.Logger;
 
 public class TeamController implements Initializable {
-    private static final Logger log = LogManager.getLogManager().getLogger(String.valueOf(TeamController.class));  //hope its right
+    //private static final Logger log = LogManager.getLogManager().getLogger(String.valueOf(TeamController.class));  //nope
+    //private static final Logger log = LogManager.getLogger(TeamController.class);
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        log.info("initialised");
+        //log.info("initialised");
     }
 }
