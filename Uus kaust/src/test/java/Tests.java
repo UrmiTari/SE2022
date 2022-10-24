8e2167933f57b9d07691ae607dae68b42f939d3a
pirjo2 <pirjo.joelo@gmail.com>
2022-10-24 13:34:05 +0300

    TESTS

---
+++ Uus kaust/src/test/java/Tests.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+public class Tests {
+    @Test
+    public void testAddingItemBeginsAndCommitsTransaction () {
+        //check that methods
+        //beginTransaction and commitTransaction are both called exactly once and
+        //that order
+
+    }
+    public void testAddingNewItem () {
+        //check that a new item is saved through the DAO
+    }
+    public void testAddingExistingItem () {
+        //check that adding a new item increases the quantity
+        //and the saveStockItem method of the DAO is not called
+    }
+    public void testAddingItemWithNegativeQuantity () {
+        //check that adding an item with
+        //negative quantity results in an exception
+    }
+}
