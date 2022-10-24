b4f0ed81efe15dbf642d7ea04b2d8625378df5c5
pirjo2 <pirjo.joelo@gmail.com>
2022-10-24 12:04:17 +0300

    DEBUG message

--- Uus kaust/SaleSystemGUI/src/main/java/ee/ut/math/tvt/salessystem/ui/controllers/StockController.java
+++ Uus kaust/SaleSystemGUI/src/main/java/ee/ut/math/tvt/salessystem/ui/controllers/StockController.java
@@ -8,14 +8,16 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
+import org.apache.logging.log4j.LogManager;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Logger;
 
 public class StockController implements Initializable {
 
     private final SalesSystemDAO dao;
-
+    //private static final Logger log = LogManager.getLogger(StockController.class);
     @FXML
     private Button addItem;
     @FXML
@@ -27,6 +29,7 @@ public class StockController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        //log.info("initialize");
         refreshStockItems();
         // TODO refresh view after adding new items
     }
