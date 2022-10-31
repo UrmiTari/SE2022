b6ae5e1c3d09ee3ddc68ec682d23229f513d0919
pirjo2 <pirjo.joelo@gmail.com>
2022-10-31 10:52:23 +0200

    Test file now work

--- Uus kaust/src/test/java/Tests.java
+++ Uus kaust/src/test/java/Tests.java
@@ -10,13 +10,16 @@ public class Tests {
         //that order
 
     }
+    @Test
     public void testAddingNewItem () {
         //check that a new item is saved through the DAO
     }
+    @Test
     public void testAddingExistingItem () {
         //check that adding a new item increases the quantity
         //and the saveStockItem method of the DAO is not called
     }
+    @Test
     public void testAddingItemWithNegativeQuantity () {
         //check that adding an item with
         //negative quantity results in an exception
