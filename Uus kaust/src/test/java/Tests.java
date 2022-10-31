615e7816daf4db528fbca90ab3d3adda27983ce6
pirjo2 <pirjo.joelo@gmail.com>
2022-11-01 00:24:25 +0200

    TESTS

--- Uus kaust/src/test/java/Tests.java
+++ Uus kaust/src/test/java/Tests.java
@@ -1,15 +1,19 @@
 //import static org.junit.Assert.assertEquals;
 
-import org.junit.After;
+import ee.ut.math.tvt.salessystem.dao.InMemorySalesSystemDAO;
+import ee.ut.math.tvt.salessystem.dao.SalesSystemDAO;
+import ee.ut.math.tvt.salessystem.dataobjects.StockItem;
+import ee.ut.math.tvt.salessystem.logic.ShoppingCart;
 import org.junit.Test;
+import org.mockito.Mockito;
 
-import java.util.List;
-
-import static org.mockito.Mockito.mock;
-//import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.*;
 
 public class Tests {
-    List mockedList = mock(List.class);
+    //List mockedList = mock(List.class);
+    private SalesSystemDAO dao;
+    private ShoppingCart shoppingCart;
+    private StockItem bread;
     @Test
     public void testAddingItemBeginsAndCommitsTransaction () {
         //check that methods
@@ -17,11 +21,24 @@ public class Tests {
         //that order
         //verify(, calls(1)).beginTransaction();   //find in shoppingcart.java
         //verify(, times(1)).commitTransaction();
+        bread = new StockItem(5L,"Bread","Fresh and fluffy", 0.59,10);
+        //dao = new InMemorySalesSystemDAO();
+        //shoppingCart = new ShoppingCart(dao);
+        shoppingCart = new ShoppingCart(dao);
+        dao = Mockito.mock(InMemorySalesSystemDAO.class);
+        verify(dao,times(1)).beginTransaction();
+        verify(dao,times(1)).commitTransaction();
     }
-    @After
+    @Test
     public void testAddingNewItem () {
         //check that a new item is saved through the DAO
-
+        bread = new StockItem(5L,"Bread","Fresh and fluffy", 0.59,10);
+        //dao = new InMemorySalesSystemDAO();
+        //mock(dao.saveStockItem(bread));
+        dao = Mockito.mock(InMemorySalesSystemDAO.class);
+        //dao.saveStockItem(bread);
+        //shoppingCart = new ShoppingCart(dao);
+        verify(dao).saveStockItem(bread);
     }
     @Test
     public void testAddingExistingItem () {
