bef92fb554fabc07f0a1566e4465804a44f7071c
DianaPri <diana980104@gmail.com>
2022-11-26 17:25:46 +0200

    Update in warehouse (notification, deleteButtonClicked,addProductButtonClicked)

--- Uus kaust/SaleSystemGUI/src/main/java/ee/ut/math/tvt/salessystem/ui/controllers/StockController.java
+++ Uus kaust/SaleSystemGUI/src/main/java/ee/ut/math/tvt/salessystem/ui/controllers/StockController.java
@@ -7,9 +7,7 @@ import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -32,6 +30,8 @@ public class StockController implements Initializable {
     private TextField nameField;
     @FXML
     private TextField priceField;
+    @FXML
+    private TextField location;
     @FXML
     private Button addToWarehouse;
     @FXML
@@ -83,6 +83,75 @@ public class StockController implements Initializable {
     private void refreshStockItems() {
         warehouseTableView.setItems(FXCollections.observableList(dao.findStockItems()));
         warehouseTableView.refresh();
+    }
+
+    private void createNotification(String message){
+        Alert notification = new Alert(Alert.AlertType.NONE,
+                message,
+                ButtonType.OK);
+        notification.setTitle("Notification!");
+        notification.showAndWait();
+    }
+
+    public void addProductButtonClicked(){
+        long id = 0;
+
+        try{
+            id = Long.parseLong(barCodeField.getText());
+        } catch (NumberFormatException e) {
+            createNotification("Barcode should be a number!");
+            return;
+        }
+
+        StockItem searchedItem = dao.findStockItem(id);
+
+        String name2 = nameField.getText();
+        String loc = location.getText();
+        double price2 = 0;
+        int quantity2 = 0;
+
+        try {
+            price2 = Double.parseDouble(priceField.getText());
+        } catch (NumberFormatException e){
+            createNotification("Price should be a number!");
+            return;
+        }
+
+        try {
+            quantity2 = Integer.parseInt(quantityField.getText());
+        } catch (NumberFormatException e){
+            createNotification("Amount should be a number!");
+            return;
+        }
+        //Check if the item that is adde to the warehouse is already there
+        if (searchedItem != null)
+            searchedItem.setQuantity(searchedItem.getQuantity() + Integer.parseInt(quantityField.getText()));
+        else {
+            StockItem newItem = new StockItem(id, name2, "", price2, quantity2,loc);
+            dao.saveStockItem(newItem);
+        }
+        barCodeField.clear();
+        quantityField.clear();
+        nameField.clear();
+        priceField.clear();
+        location.clear();
+
+        log.info("Product added to the warehouse.");
+    }
+
+    public void deleteButtonClicked(){ deleteStockItem();}
+
+    private void deleteStockItem(){
+        int selectedID = warehouseTableView.getSelectionModel().getSelectedIndex();
+        if (selectedID >= 0){
+            warehouseTableView.getItems().remove(selectedID);
+            warehouseTableView.getSelectionModel().clearSelection();
+            log.info("Product deleted");
+        } else if (warehouseTableView.getItems().size() == 0){
+            createNotification("The table is empty. Nothing to delete.");
+        } else{
+            createNotification("Select a table row to delete it.");
+        }
     }
     /** Event handler for the <code>new purchase</code> event. */
     /**@FXML
